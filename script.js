929300edfc923bf24176fb369ab8b8c20fd2511e
joshtrinh1102 <nltrinh1102@gmail.com>
2024-06-07 22:48:42 -0400

    Implement form validation and submission logic.

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    let feedback= document.querySelector('feedback')
+});
+let submit= document.querySelector("submit")
+submit.addEventListener("click", event=>{
+    event.preventDefault()})
+    //prevent default;
+
+    
+feedbackDisplay.appendchild(feedback)
+//Implement form validation and submission logic.
